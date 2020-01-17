ec1ae5fb11445ae254c7542b42872d8d8733ad5a
MarosiF <49545584+MarosiF@users.noreply.github.com>
2020-01-17 20:48:46 +0100

    frontend user and shopping cart mod, ent, service

--- 02-frontend/angular-coffeecat/src/app/app.module.ts
+++ 02-frontend/angular-coffeecat/src/app/app.module.ts
@@ -5,17 +5,25 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
+import { UserService } from './services/user.service';
+import { ShoppingcartService } from './services/shoppingcart.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    UserListComponent,
+    ShoppingcartComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
   ],
   providers: [ProductService],
+  //providers: [UserService],
+  //providers: [ShoppingcartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
